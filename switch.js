96ff173559541480ef98d1738b6fde4f36d60a57
11gargiigit <11gargisingh@gmail.com>
2024-12-19 16:25:49 +0530

    operate

--- switch.js
+++ switch.js
@@ -1,18 +1,18 @@
-// var grade = "A";
-// document.write("entering switch block:");
-// switch(grade) {
-//   case 'A': document.write("good job");
-//   break;
-//   case 'B': document.write("pretty good job");
-//   break;
-//   case 'C': document.write("decent job");
-//   break;
-//   case 'D': document.write("not so good job");
-//   break;
-//   case 'F': document.write("poor job");
-//   break;
-//   default : document.write("unidentified grade");
-// }
+var grade = "A";
+document.write("entering switch block:");
+switch(grade) {
+  case 'A': document.write("good job");
+  break;
+  case 'B': document.write("pretty good job");
+  break;
+  case 'C': document.write("decent job");
+  break;
+  case 'D': document.write("not so good job");
+  break;
+  case 'F': document.write("poor job");
+  break;
+  default : document.write("unidentified grade");
+}
 
 
 const square = function (number) {
